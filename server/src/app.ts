78aa8650baf9a0a82526e76e5a1279f0ef934d78
CJ-Yang0225 <cj.yang0225@gmail.com>
2021-05-15 22:37:26 +0800

    chore: set express & connect to MongoDB

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,28 @@
+import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+import postRoutes from "./routes/posts";
+
+dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Test");
-});
+app.use(express.json({ limit: "30mb" }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
-app.listen(5000, () => console.log("Server running..."));
+const URI = process.env.MONGODB_CONNECTION_URI || "";
+const PORT = process.env.PORT || 5000;
+
+app.use("/posts", postRoutes);
+
+mongoose
+  .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log("Server is running on port:", PORT))
+  )
+  .catch((error) => console.error(`${error} did not connect!`));
+
+mongoose.set("useFindAndModify", false);
