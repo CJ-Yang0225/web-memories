53a816bced9caa06d4b56fabd0da8b12fefa47b6
ChaoJie Yang <57297482+CJ-Yang0225@users.noreply.github.com>
2021-06-03 22:43:51 +0800

    feat: add combineReducers and post reducer

---
+++ client/src/reducers/posts.ts
@@ -0,0 +1,35 @@
+import { Posts } from "../api";
+import {
+  CREATE_POST,
+  DELETE_POST,
+  GET_ALL_POSTS,
+  UPDATE_POST,
+} from "../constants/actionTypes";
+
+type Action = {
+  type: string;
+  payload: any;
+};
+
+const reducer = (posts: Posts = [], action: Action) => {
+  switch (action.type) {
+    case GET_ALL_POSTS:
+      return action.payload;
+
+    case CREATE_POST:
+      return [...posts, action.payload];
+
+    case UPDATE_POST:
+      return posts.map((post) =>
+        post._id === action.payload._id ? action.payload : post
+      );
+
+    case DELETE_POST:
+      return posts.find((post) => post !== action.payload._id);
+
+    default:
+      return posts;
+  }
+};
+
+export default reducer;
