51cf2734937e7b247568767f5b32d7f2aa868357
ChaoJie Yang <57297482+CJ-Yang0225@users.noreply.github.com>
2021-06-17 23:59:56 +0800

    feat: finish PostCardList and PostCard

--- client/src/components/Post/PostCard.tsx
+++ client/src/components/Post/PostCard.tsx
@@ -1,22 +1,109 @@
+import React from "react";
+import moment from "moment";
 import styled from "@emotion/styled";
-import { Grid } from "@material-ui/core";
+import {
+  Card,
+  CardMedia,
+  Typography,
+  Button,
+  CardContent,
+  CardActions,
+} from "@material-ui/core";
+import { Edit } from "@material-ui/icons";
 
 import { Post } from "../../api";
 
 type Props = {
-  post?: Post;
+  post: Post;
 };
+
+const defaultImage =
+  "https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png";
 
 function PostCard({ post }: Props) {
   return (
-    <Card item sm={6} xs={12}>
-      <div className="card__content">Post</div>
-    </Card>
+    <Container>
+      <Thumbnail image={post.selectedFile || defaultImage} title={post.title} />
+      <Overlay top="16px" left="16px">
+        <Typography variant="h6">{post.creator}</Typography>
+        <Typography variant="body2">
+          {moment(post.createdAt).fromNow()}
+        </Typography>
+      </Overlay>
+      <Overlay top="16px" right="16px">
+        <Button style={{ color: "white" }}>
+          <Edit />
+        </Button>
+      </Overlay>
+      <TagBox>
+        <Typography
+          variant="body2"
+          color="textSecondary"
+          component="h2"
+        >{post.tags.map(tag => `#${tag} `)}</Typography>
+      </TagBox>
+      <Title variant="h5" component="h2">
+        {post.title}
+      </Title>
+      <CardContent>
+        <Typography variant="body2" component="p">
+          {post.message}
+        </Typography>
+      </CardContent>
+      <CardActions></CardActions>
+    </Container>
   );
 }
 
-const Card = styled(Grid)`
+const Container = styled(Card)`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border-radius: 16px;
+  height: 100%;
+`;
 
+const Thumbnail = styled(CardMedia)`
+  padding-top: 56.25%;
+  background-color: rgba(0, 0, 0, 0.5);
+  background-blend-mode: darken;
+`;
+
+type OverlayProps = {
+  top?: string;
+  left?: string;
+  right?: string;
+};
+
+const Overlay = styled.div<OverlayProps>`
+  position: absolute;
+  top: ${({ top }) => top};
+  left: ${({ left }) => left};
+  right: ${({ right }) => right};
+  color: white;
+
+  .MuiButton-root {
+    border-radius: 100%;
+    padding: 8px;
+    min-width: min-content;
+
+    &:hover {
+      background-color: #0000003d;
+    }
+  }
+`;
+
+const Title = styled(Typography)<{ component: React.ElementType }>`
+  display: flex;
+  align-items: center;
+  padding: 0 16px;
+`;
+
+const TagBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 16px;
 `;
 
 export default PostCard;
--- client/src/components/Post/PostCardList.tsx
+++ client/src/components/Post/PostCardList.tsx
@@ -2,20 +2,22 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 import PostCard from "./PostCard";
-import { Grid } from "@material-ui/core";
+import { Grid, CircularProgress } from "@material-ui/core";
 import { RootState } from "../../store";
 
 function PostCardList() {
   const posts = useSelector((state: RootState) => state.postsReducer);
+  const isLoading = posts.length === 0;
+  const renderPosts = () =>
+    posts.map((post) => (
+      <Grid key={post._id} item sm={6} md={6} xs={12}>
+        <PostCard post={post} />
+      </Grid>
+    ));
 
-  console.log(posts);
   return (
     <Grid container spacing={3}>
-      {posts.map((post) => (
-        <Grid key={post._id} item sm={6} md={6} xs={12}>
-          <PostCard post={post} />
-        </Grid>
-      ))}
+      {isLoading ? <CircularProgress /> : renderPosts()}
     </Grid>
   );
 }
